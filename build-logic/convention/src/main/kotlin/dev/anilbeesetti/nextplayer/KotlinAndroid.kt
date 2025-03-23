f5ecfe5edc0bce62e94a6c05bb228ac10fa91c96
Bitlinker <bitlinker@gmail.com>
2025-03-23 15:28:53 +0100

    bump java

--- build-logic/convention/src/main/kotlin/dev/anilbeesetti/nextplayer/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/dev/anilbeesetti/nextplayer/KotlinAndroid.kt
@@ -22,8 +22,8 @@ internal fun Project.configureKotlinAndroid(
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_17
-            targetCompatibility = JavaVersion.VERSION_17
+            sourceCompatibility = JavaVersion.VERSION_21
+            targetCompatibility = JavaVersion.VERSION_21
         }
 
         configureKotlin()
@@ -33,8 +33,7 @@ internal fun Project.configureKotlinAndroid(
 fun Project.configureKotlin() {
     tasks.withType<KotlinCompile>().configureEach {
         kotlinOptions {
-            /// Set JVM target to 17
-            jvmTarget = JavaVersion.VERSION_17.toString()
+            jvmTarget = JavaVersion.VERSION_21.toString()
         }
     }
 }
